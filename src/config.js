65aa3e8130b2234759d3f8278e3abf255204e1d7
drashti1212 <drashtivaghasiya22@gmail.com>
2023-09-01 23:20:46 +0100

    url changes

---
+++ src/config.js
@@ -0,0 +1,9 @@
+let config = '';
+
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development') {
+    config = {
+        SERVER_URL: "https://social-media-hiix669o4-drashti1212.vercel.app/"
+    }
+}
+
+export default config;
--- src/scenes/widgets/PostWidget.jsx
+++ src/scenes/widgets/PostWidget.jsx
@@ -11,7 +11,7 @@ import WidgetWrapper from "components/WidgetWrapper";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setPost } from "state";
-
+import config from "../../config"
 const PostWidget = ({
   postId,
   postUserId,
@@ -30,12 +30,13 @@ const PostWidget = ({
   const isLiked = Boolean(likes[loggedInUserId]);
   const likeCount = Object.keys(likes).length;
 
+
   const { palette } = useTheme();
   const main = palette.neutral.main;
   const primary = palette.primary.main;
 
   const patchLike = async () => {
-    const response = await fetch(`http://localhost:3001/posts/${postId}/like`, {
+    const response = await fetch(`${config.SERVER_URL}/posts/${postId}/like`, {
       method: "PATCH",
       headers: {
         Authorization: `Bearer ${token}`,
@@ -64,7 +65,7 @@ const PostWidget = ({
           height="auto"
           alt="post"
           style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
-          src={`http://localhost:3001/assets/${picturePath}`}
+          src={`${config.SERVER_URL}/assets/${picturePath}`}
         />
       )}
       <FlexBetween mt="0.25rem">
